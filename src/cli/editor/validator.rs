08ead55ecf6992f119018a8d7f057bef53ef64c5
George Lewis <georgejeffreylewis@gmail.com>
2021-06-14 16:35:54 -0400

    remove deadcode

--- src/cli/editor/validator.rs
+++ src/cli/editor/validator.rs
@@ -7,23 +7,10 @@ impl Validator for MyHelper<'_> {
         &self,
         _ctx: &mut rustyline::validate::ValidationContext,
     ) -> rustyline::Result<ValidationResult> {
-        // let line = ctx.input();
-        // let context = EvaluationContext::default();
-
-        // // dbg!(line);
-
-        // let (valid_, result) = if tokenize(line, &context).is_err() {
-        //     (false, ValidationResult::Incomplete)
-        // } else {
-        //     (true, ValidationResult::Valid(None))
-        // };
-        // *self.valid.borrow_mut() = valid_;
-        // rustyline::Result::Ok(result)
         Ok(rustyline::validate::ValidationResult::Valid(None))
     }
 
     fn validate_while_typing(&self) -> bool {
-        // true
         false
     }
 }
