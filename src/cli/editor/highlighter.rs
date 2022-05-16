4ecf041dcc169f68d172326f9d9bd621c7d6f0d4
George Lewis <33588728+George-lewis@users.noreply.github.com>
2022-05-16 01:51:56 -0400

    initial func assign highlight impl

--- src/cli/editor/highlighter.rs
+++ src/cli/editor/highlighter.rs
@@ -1,16 +1,84 @@
 // Rustyline forces us
-use std::borrow::Cow;
+use std::{borrow::Cow, ops::Index};
 
 use colored::Colorize;
+use itertools::Itertools;
 use rustmatheval::{
-    model::{errors::ErrorContext, EvaluationContext},
+    model::{errors::ErrorContext, functions::Function, variables::Variable, EvaluationContext},
     tokenize,
+    utils::{self, Pos},
 };
 use rustyline::highlight::Highlighter;
 
-use crate::stringify::stringify;
+use crate::{
+    funcs::{color_arg, format_func_name},
+    stringify::stringify,
+    vars::format_var_name,
+};
 
 use super::MyHelper;
+
+struct Arg<'s> {
+    repr: &'s str,
+    offset: usize,
+}
+
+fn parse_func_def<'s>(string: &'s str) -> (&str, Vec<Arg>) {
+    let fname = string.split_whitespace().next().unwrap();
+    let mut args = vec![];
+
+    let mut idx = fname.len();
+    let mut start = None;
+    let mut white = 0;
+
+    let mut push_arg = |string: &'s str,
+                        start_: usize,
+                        end: &Pos,
+                        start: &mut Option<usize>,
+                        white: &mut usize| {
+        let repr = utils::slice(string, start_, end);
+        args.push(Arg {
+            repr,
+            offset: *white,
+        });
+        *start = None;
+        *white = 0;
+    };
+
+    while idx < string.len() {
+        if string.chars().nth(idx).unwrap().is_whitespace() {
+            if let Some(start_) = start {
+                push_arg(string, start_, &Pos::Idx(idx), &mut start, &mut white);
+            }
+            white += 1;
+        } else if start.is_none() {
+            start = Some(idx);
+        }
+        idx += 1;
+    }
+
+    if let Some(start_) = start {
+        push_arg(string, start_, &Pos::Idx(idx), &mut start, &mut white);
+    }
+
+    (&fname[1..], args)
+}
+
+fn _format_func_def(fname: &str, args: Vec<Arg>) -> String {
+    let mut fmt = format_func_name(fname).to_string();
+
+    for arg in args {
+        fmt.push_str(&" ".repeat(arg.offset));
+        fmt.push_str(&color_arg(arg.repr).to_string());
+    }
+
+    fmt
+}
+
+fn format_fun_def(def: &str) -> String {
+    let (fname, args) = parse_func_def(def);
+    _format_func_def(fname, args)
+}
 
 impl Highlighter for MyHelper<'_> {
     fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
@@ -26,10 +94,57 @@ impl Highlighter for MyHelper<'_> {
             context: ErrorContext::Main,
             depth: 0,
         };
-        let string = match tokenize(line, &context) {
+
+        let stringify = |line: &'l str| match tokenize(line, &context) {
             Ok(tokens) => stringify(&tokens),
             Err(tokens) => stringify(&tokens),
         };
+
+        if line.contains('=') {
+            let split: Vec<&str> = line.split('=').collect();
+            if split.len() != 2 {
+                // Bail
+                return Cow::Borrowed(line);
+            }
+
+            let left = split[0];
+            let right = split[1];
+
+            let not_whitespace = |idx: usize, c: char| {
+                if c.is_whitespace() {
+                    None
+                } else {
+                    Some(idx)
+                }
+            };
+
+            let lspace = left
+                .char_indices()
+                .rev()
+                .find_map(|(idx, c)| not_whitespace(idx, c))
+                .map_or(0, |x| left.chars().count() - x - 1);
+            let rspace = right
+                .char_indices()
+                .find_map(|(idx, c)| not_whitespace(idx, c))
+                .unwrap_or(0);
+
+            let left = if Function::is(line) {
+                format_fun_def(left)
+            } else if Variable::is(line) {
+                format_var_name(&left[1..]).to_string()
+            } else {
+                String::new()
+            };
+
+            let right = stringify(right);
+
+            return Cow::Owned(format!(
+                "{left}{}={}{right}",
+                " ".repeat(lspace),
+                " ".repeat(rspace)
+            ));
+        }
+        let string = stringify(line);
         Cow::Owned(string)
     }
 
